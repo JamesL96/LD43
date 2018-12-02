f0ba667b1693ecb184694916c8ac7d2b84f19937
Alessandro <alessandro.fama@ymail.com>
2018-12-02 15:27:31 +0100

    music and parameter setting first test

--- LD43/Assets/_NOT FMOD/Scripts/Audio/AudioManager.cs
+++ LD43/Assets/_NOT FMOD/Scripts/Audio/AudioManager.cs
@@ -6,6 +6,7 @@ public class AudioManager : MonoBehaviour
 {
     public static AudioManager instance;
     private static FMOD.Studio.EventInstance ambienceInstance;
+    private static FMOD.Studio.EventInstance musicInstance;
 
 
     void Awake()
@@ -27,10 +28,32 @@ public class AudioManager : MonoBehaviour
     void Start()
     {
         ambienceInstance = FMODUnity.RuntimeManager.CreateInstance(FMODPaths.AMBIENCE);
+        musicInstance = FMODUnity.RuntimeManager.CreateInstance(FMODPaths.MUSIC);
         PlayAmbience();
     }
 
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Keypad1))
+        {
+            PlayMusic();
+        }
 
+        if (Input.GetKey(KeyCode.Keypad0))
+        {
+            StopMusic();
+        }
+
+        if (Input.GetKey(KeyCode.Keypad2))
+        {
+            SetMusicParameter(musicInstance, FMODPaths.CREW_NUMBER, 5);
+        }
+
+        if (Input.GetKey(KeyCode.Keypad3))
+        {
+            SetMusicParameter(musicInstance, FMODPaths.CREW_NUMBER, 15);
+        }
+    }
 
     public void PlayAmbience()
     {
@@ -44,5 +67,34 @@ public class AudioManager : MonoBehaviour
 
     }
 
+    public void PlayMusic()
+    {
+
+        FMOD.Studio.PLAYBACK_STATE _playing;
+        musicInstance.getPlaybackState(out _playing);
+        if (_playing != FMOD.Studio.PLAYBACK_STATE.PLAYING)
+        {
+            musicInstance.start();
+        }
+
+    }
+
+    public void StopMusic()
+    {
+        musicInstance.stop(FMOD.Studio.STOP_MODE.ALLOWFADEOUT);
+        musicInstance.release();
+    }
+
+
+    void SetMusicParameter(FMOD.Studio.EventInstance musicInstance, string name, int value)
+    {
+        FMOD.Studio.ParameterInstance parameter;
+        musicInstance.getParameter(name, out parameter);
+
+        parameter.setValue(value);
+    }
 
 }
+
+
+
--- LD43/Assets/_NOT FMOD/Scripts/Audio/FMODPaths.cs
+++ LD43/Assets/_NOT FMOD/Scripts/Audio/FMODPaths.cs
@@ -14,9 +14,12 @@
     public static readonly string PICK_UP = "event:/general/pick_up";
 
     public static readonly string AMBIENCE = "event:/ambience/ambience";
+    public static readonly string MUSIC = "event:/music/music";
 
 
     // PARAMETERS
+
+    public static readonly string CREW_NUMBER = "CREW";
 
     // SNAPSHOTS
 
