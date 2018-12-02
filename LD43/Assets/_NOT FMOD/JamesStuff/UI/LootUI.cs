41a303d4fe01b73f7c661ddb0967d5ecd88ead42
JamesL96 <jameslongo@email.com>
2018-12-01 20:02:47 -0500

    James changes

---
+++ LD43/Assets/_NOT FMOD/JamesStuff/UI/LootUI.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LootUI : MonoBehaviour {
+    public float lootWeight;
+    public List<GameObject> boatLoot; 
+
+	void Update ()
+    {
+        boatLoot.Clear();
+        foreach (GameObject bo in FindObjectOfType<Boat>().boatObjs)
+            if (bo.GetComponent<Loot>() && !boatLoot.Contains(bo))
+                boatLoot.Add(bo);
+
+        lootWeight = 0;
+        foreach (GameObject bl in boatLoot)
+            lootWeight += bl.GetComponent<Rigidbody>().mass * 100;
+
+        GetComponent<Image>().fillAmount = lootWeight / FindObjectOfType<GameManager>().maxWeight;
+        GameObject.Find("LootTargetText").GetComponent<Text>().text = "Loot: " + Mathf.RoundToInt((lootWeight / FindObjectOfType<GameManager>().maxWeight) * 100) + "%";
+
+    }
+}
