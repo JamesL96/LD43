0e9c123ff7dd53b0bbdf36606cf0073963e60828
DavidDePauw <daviddep@buffalo.edu>
2018-12-01 19:02:31 -0500

    Working on cannons
    cannons

---
+++ LD43/Assets/_NOT FMOD/DavidStuff/Cannon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour {
+    public GameObject target;
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        GetComponent<Rigidbody>().isKinematic = true;
+        float dx = Vector2.Distance(new Vector2(transform.position.x,transform.position.z), new Vector2(target.transform.position.x,target.transform.position.z));
+        float Vx = Vector3.ProjectOnPlane(transform.forward.normalized, Vector3.up).magnitude*speed;
+        float d = .5f * Physics.gravity.magnitude * Mathf.Pow(dx / Vx, 2);
+        transform.LookAt(target.transform.position + Vector3.up*d);
+        transform.Rotate(Vector3.left * 90f);
+	}
+}
